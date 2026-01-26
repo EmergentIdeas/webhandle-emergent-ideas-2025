71917ced30d7d3e48560aa6fa6bea1851c1985f6
Dan Kolz <dan@emergentideas.com>
2026-01-26 15:24:25 -0600

    add ability to run a bit of code on start

--- environment-start.mjs
+++ environment-start.mjs
@@ -1,3 +1,12 @@
 #! /usr/local/bin/node
 import createEnvironment from './create-environment.mjs';
-createEnvironment()
+let webhandle = await createEnvironment()
+
+let serverStartingPoint = process.argv[2]
+if(serverStartingPoint) {
+	import(serverStartingPoint).then(mod => {
+		if(mod && mod.default) {
+			mod.default(webhandle)
+		}
+	})
+}
